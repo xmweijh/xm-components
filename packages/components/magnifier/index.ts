fe35592141d747dc87f47e83335e5038263ad10c
LC10230327 <1931445103@qq.com>
2023-02-23 16:16:34 +0800

    feat: ✨ 放大镜组件

---
+++ packages/components/magnifier/index.ts
@@ -0,0 +1,6 @@
+import { withInstall } from '../../utils/install';
+import magnifier from './src/magnifier.vue';
+
+export const IMagnifier = withInstall(magnifier); // 增加类型
+
+export default IMagnifier;
